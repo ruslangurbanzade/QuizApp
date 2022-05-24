d347e93368dbf64a3a0ae734a01298b693702e61
ruslangurbanzade <ruslan.gurbanzade@hellofresh.com>
2022-05-24 10:44:58 +0200

    first commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.quizapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quizapp.ui.theme.QuizAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QuizAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentWidth(align = Alignment.CenterHorizontally)
+                        .wrapContentHeight(align = Alignment.CenterVertically),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    var visible by remember { mutableStateOf(true) }
+    AnimatedVisibility(visible = visible) {
+        TextButton(onClick = { visible = false }) {
+
+            Text(text = "Hello $name!")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    QuizAppTheme {
+        Greeting("Android")
+    }
+}
